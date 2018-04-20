c91f8b7cbd885f77f3a5ea249525865ddf2aeec0
Pierre <zins.pierre@gmail.com>
2018-04-20 18:34:30 -0400

    save

--- scripts/grpc_worker.sh
+++ scripts/grpc_worker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+other_computer_ip="192.168.1.3"
+other_computer_ip="132.207.72.22"
 tf_program_name="cnn_distributed"
 scripts_dir=`pwd`
 
@@ -7,7 +9,8 @@ bash start_tracing.sh -g
 
 bash set_env.sh --hip --hc 1
 cd /home/pierre/Dropbox/dev/distributed/in_model_parallelism/
-python3 $tf_program_name.py w > /dev/null 2>&1
+python3 $tf_program_name.py w
+# python3 $tf_program_name.py w > /dev/null 2>&1
 
 
 cd $scripts_dir
@@ -16,6 +19,6 @@ bash post_process.sh -s -t
 
 kernel_traces=`ls -t ../lttng-traces | head -1`
 
-ssh -A 192.168.1.3 mkdir ~/traces_grpc
-scp -r ../lttng-traces/$kernel_traces 192.168.1.3:~/traces_grpc/"$kernel_traces"_remote
-scp -r ../results 192.168.1.3:~/traces_grpc/results_remote
+ssh -A $other_computer_ip mkdir ~/traces_grpc
+scp -r ../lttng-traces/$kernel_traces "$other_computer_ip":~/traces_grpc/"$kernel_traces"_remote
+scp -r ../results "$other_computer_ip":~/traces_grpc/results_remote
